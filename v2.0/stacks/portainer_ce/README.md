7fd8463269301cad61d59165b58bd5a347a65d6e
John Chambers-Malewig <johncm@rcn.com>
2021-02-20 17:59:49 +0000

    Add portainer-ce service files

---
+++ v2.0/stacks/portainer_ce/README.md
@@ -0,0 +1,89 @@
+# Run Portainer and Agent via docker-compose on Linux
+- Install docker and docker-compose:
+```
+sudo apt update
+sudo apt install docker.io docker-compose
+```
+
+- Add your user into `docker` group and then re-login to your account:
+```
+sudo usermod -aG docker <your_user>
+```
+
+# TLDR; Simple Copy/Paste
+- Run the following commands to deploy Portainer in your Docker host.
+
+```
+docker volume create portainer_data
+```
+```
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent
+```
+```
+docker run -d -p 9000:9000 --name portainer_ce --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce -H unix:///var/run/docker.sock
+
+```
+
+# Customized using a script
+- Run Portainer and Agent:
+
+```
+bash deploy.sh up [<PORTAINER_PUBLISHED_PORT> [<PORTAINER_PORT> [<AGENT_PUBLISHED_PORT> [<AGENT_PORT> [<AGENT_HOST> [<AGENT_SECRET> [<TZ>]]]]]]]
+```
+where:
+- "PORTAINER_PUBLISHED_PORT"-"Port Portainer will be accessible on. (Defaults to 9000)",
+- "PORTAINER_PORT"-"Port Portainer will run on within the container. (Defaults to 9000)",
+- "AGENT_PUBLISHED_PORT"-"Port Agent will be accessible on. (Defaults to 9001)",
+- "AGENT_PORT"-"Port Agent will run on within the container. (Defaults to 9001)",
+- "AGENT_HOST"-"Host Agent will listen on within the container. (Defaults to 0.0.0.0)",
+- "AGENT_SECRET"-"Agent Secret used by Portainer to connect to Agent from multiple sources."
+- "TZ"-"Timezone Portainer will run in."
+
+for example:
+
+```
+bash deploy.sh up 9000
+```
+
+- Stop Portainer Agent:
+```
+bash deploy.sh down
+```
+
+# Note: Portainer and Agent via docker-compose on RancherOS
+- Docker is already installed on RancherOS but lacks **docker-compose** with a basic installation, this script will use a utility container instead:
+  - docker/compose
+
+- RancherOS also lacks **curl** with a basic installation, use a utility container to download files instead:
+  - curlimages/curl
+
+Once logged in as "rancher" user:
+- To Run Portainer Agent:
+
+```
+docker run --rm curlimages/curl -L -v https://templesofsyrinx.github.io/portainer-templates/v2.0/stacks/portainer_ce/docker-compose.yml > docker-compose.yml
+
+docker run --rm curlimages/curl -L -v https://templesofsyrinx.github.io/portainer-templates/v2.0/stacks/portainer_ce/deploy.sh > deploy.sh
+```
+then:
+```
+bash deploy.sh up <optional parameters>
+```
+# Note: Portainer via docker-compose on Synology
+- Install the **Docker** Package using **Package Center**
+
+- The Synology **Docker** Package places the volumes in a custom location, **/volume1/@docker/volumes**
+- Use a different compose file to deploy on Synology
+
+Once logged in as "root" user:
+- To Run Portainer and Agent:
+
+```
+curl -L https://templesofsyrinx.github.io/portainer-templates/v2.0/stacks/portainer_ce/docker-compose-syno.yml -o docker-compose.yml
+
+curl -L https://templesofsyrinx.github.io/portainer-templates/v2.0/stacks/portainer_ce/deploy.sh -o deploy.sh
+```
+then:
+```
+bash deploy.sh up <optional parameters>
+```
---
+++ v2.0/stacks/portainer_ce/deploy.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+
+set -eu
+# set -x
+
+: ${1?"Usage: $0 <ACTION> [<PORTAINER_PUBLISHED_PORT> [<PORTAINER_PORT> [<AGENT_PUBLISHED_PORT> [<AGENT_PORT> [<AGENT_HOST> [<AGENT_SECRET> [<TZ>]]]]]]]"}
+ACTION=$1 # up, down
+
+if [ "$ACTION" = "up" ]
+then
+    # Construct a string of environemnt variables if we need it
+    ENV_VARIABLES=
+
+    # Portainer deployment options:
+    PORT=9000
+
+    # Agent deployment options:
+    DEFAULT_PORT=9001
+
+    # Portainer Published Port
+    # If PORTAINER_PUBLISHED_PORT is unset (does not exist in the environment) or is empty, expect it as the second parameter and export.
+    # If no second parameter, set to default
+    export PORTAINER_PUBLISHED_PORT=${PORTAINER_PUBLISHED_PORT:-${2:-${PORT}}}
+    # TODO: check that it is within a valid port range
+    ENV_VARIABLES="${ENV_VARIABLES} -e PORTAINER_PUBLISHED_PORT=${PORTAINER_PUBLISHED_PORT}"
+    # Show variable in the environment
+    env | grep PORTAINER_PUBLISHED_PORT
+
+    # Portainer Port
+    # If PORTAINER_PORT is unset (does not exist in the environment) or is empty, expect it as the second parameter and export.
+    # If no third parameter, set to default
+    export PORTAINER_PORT=${PORTAINER_PORT:-${3:-${PORT}}}
+    # TODO: check that it is within a valid port range
+    ENV_VARIABLES="${ENV_VARIABLES} -e PORTAINER_PORT=${PORTAINER_PORT}"
+    # Show variable in the environment
+    env | grep PORTAINER_PORT
+
+    # Agent Published Port
+    # If AGENT_PUBLISHED_PORT is unset (does not exist in the environment) or is empty, expect it as the second parameter and export.
+    # If no fourth parameter, set to default
+    export AGENT_PUBLISHED_PORT=${AGENT_PUBLISHED_PORT:-${4:-${DEFAULT_PORT}}}
+    # TODO: check that it is within a valid port range
+    ENV_VARIABLES="${ENV_VARIABLES} -e AGENT_PUBLISHED_PORT=${AGENT_PUBLISHED_PORT}"
+    # Show variable in the environment
+    env | grep AGENT_PUBLISHED_PORT
+
+    # Agent Port
+    # If AGENT_PORT is unset (does not exist in the environment) or is empty, expect it as the third parameter and export.
+    # If no fifth parameter, set to default
+    export AGENT_PORT=${AGENT_PORT:-${5:-${DEFAULT_PORT}}}
+    # TODO: check that it is within a valid port range
+    ENV_VARIABLES="${ENV_VARIABLES} -e AGENT_PORT=${AGENT_PORT}"
+    # Show variable in the environment
+    env | grep AGENT_PORT
+
+    # Agent Host
+    # If AGENT_HOST is unset (does not exist in the environment).
+    if [ -z "${AGENT_HOST+set}" ]; then
+       1>&2 echo "AGENT_HOST is unset"
+    elif [ -z "${AGENT_HOST-unset}" ]; then
+       # AGENT_HOST set but empty, don't pass an empty value to container
+       1>&2 echo "AGENT_HOST empty, unsetting"
+       unset AGENT_HOST
+    fi
+    # Check for AGENT_HOST as sixth parameter
+    if [ -z "${6+set}" ]; then
+       1>&2 echo "AGENT_HOST (parameter 6) is unset"
+    elif [ -z "${6-unset}" ]; then
+       # AGENT_HOST (6) set but empty, don't pass an empty value to container
+       1>&2 echo "AGENT_HOST (parameter 6) empty"
+    else 
+       export AGENT_HOST=${6}
+       # TODO: check that it is within a valid host range
+       # Show variable in the environment
+       env | grep AGENT_HOST
+    fi
+    if [ ! -z "${AGENT_HOST+set}" ]; then
+       ENV_VARIABLES="${ENV_VARIABLES} -e AGENT_HOST=${AGENT_HOST}"
+    fi
+    
+    # Agent Secret
+    # If AGENT_SECRET is unset (does not exist in the environment).
+    if [ -z "${AGENT_SECRET+set}" ]; then
+       1>&2 echo "AGENT_SECRET is unset"
+    elif [ -z "${AGENT_SECRET-unset}" ]; then
+       # AGENT_SECRET set but empty, don't pass an empty value to container
+       1>&2 echo "AGENT_SECRET empty, unsetting"
+       unset AGENT_SECRET
+    fi
+    # Check for AGENT_SECRET as seventh parameter
+    if [ -z "${7+set}" ]; then
+       1>&2 echo "AGENT_SECRET (parameter 7) is unset"
+    elif [ -z "${7-unset}" ]; then
+       # AGENT_SECRET (7) set but empty, don't pass an empty value to container
+       1>&2 echo "AGENT_SECRET (parameter 7) empty"
+    else 
+       export AGENT_SECRET=${7}
+       # Show variable in the environment
+       env | grep AGENT_SECRET
+    fi
+    if [ ! -z "${AGENT_SECRET+set}" ]; then
+       ENV_VARIABLES="${ENV_VARIABLES} -e AGENT_SECRET=${AGENT_SECRET}"
+    fi
+    
+    # Timezone
+    # If TZ is unset (does not exist in the environment).
+    if [ -z "${TZ+set}" ]; then
+       1>&2 echo "TZ is unset"
+    elif [ -z "${TZ-unset}" ]; then
+       # TZ set but empty, don't pass an empty value to container
+       1>&2 echo "TZ empty, unsetting"
+       unset TZ
+    fi
+    # Check for TZ as eigth parameter
+    if [ -z "${8+set}" ]; then
+       1>&2 echo "TZ (parameter 8) is unset"
+    elif [ -z "${8-unset}" ]; then
+       # TZ (6) set but empty, don't pass an empty value to container
+       1>&2 echo "TZ (parameter 8) empty"
+    else 
+       export TZ=${8}
+       # TODO: check that it is within a valid timezone
+       # Show variable in the environment
+       env | grep TZ
+    fi
+    if [ ! -z "${TZ+set}" ]; then
+       ENV_VARIABLES="${ENV_VARIABLES} -e TZ=${TZ}"
+    fi
+
+    if [ type docker-compose >/dev/null 2>&1 ]; then
+       # if docker-compose is installed, most likely docker is also
+       docker-compose ${ACTION} -d
+    else
+       echo >&2 "Script expecting docker-compose but it's not installed.  Using docker/compose container instead."
+       type docker >/dev/null 2>&1 || { echo >&2 "Script requires docker but it's not installed.  Aborting."; exit 1; }
+       docker run -ti --rm \
+       -v $(pwd):$(pwd) -v /var/run/docker.sock:/var/run/docker.sock -w $(pwd) \
+       ${ENV_VARIABLES} \
+       docker/compose ${ACTION} -d    
+    fi
+elif [ "$ACTION" = "down" ]
+then
+    if [ type docker-compose >/dev/null 2>&1 ]; then
+       # if docker-compose is installed, most likely docker is also
+       docker-compose ${ACTION}
+    else
+       echo >&2 "Script expecting docker-compose but it's not installed.  Using docker/compose container instead."
+       type docker >/dev/null 2>&1 || { echo >&2 "Script requires docker but it's not installed.  Aborting."; exit 1; }
+       docker run -ti --rm \
+       -v $(pwd):$(pwd) -v /var/run/docker.sock:/var/run/docker.sock -w $(pwd) \
+       docker/compose ${ACTION}    
+    fi
+else
+    1>&2 echo "Invalid action. Must be up or down"
+fi
